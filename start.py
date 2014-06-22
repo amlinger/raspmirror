a9710807a5abec9e4d46a310bd579d6a936ed0e1
Anton Amlinger <anton.amlinger@gmail.com>
2014-06-22 13:33:43 +0000

    Testing restart once again.

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is a kind of a magic mirror!'
+    return 'This is a KITTEN mirror. It\'s for kittens'
 
 if __name__ == '__main__':
     app.run()
