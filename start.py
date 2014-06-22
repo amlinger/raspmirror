f68df58f04dfb5a2e0b44682bdb7e2b60d88fa89
Anton Amlinger <anton.amlinger@gmail.com>
2014-06-22 12:38:16 +0000

    Testing auto-pull on restart.

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is a magic mirror!'
+    return 'This is a really really magic mirror!'
 
 if __name__ == '__main__':
     app.run()
